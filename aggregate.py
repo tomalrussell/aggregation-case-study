fbb1a05407e332add77bd9a5f02cbe6883815cc4
Tom Russell <tomalrussell@gmail.com>
2016-11-23 11:07:47 +0000

    aggregate: simpler insert to index, lookup in array

--- aggregate.py
+++ aggregate.py
@@ -4,37 +4,35 @@ from shapely.geometry import shape
 from rtree import index
 
 INPUT_GEOMETRY_FILE = 'data/oa/england_oa_2011_clipped_with_pop.shp'
-
 REPORTING_GEOMETRY_FILE = 'data/lad/england_lad_2011_clipped_norfolk.shp'
 OUTPUT_FILE = 'data/lad/england_lad_2011_clipped_norfolk_with_pop.shp'
-
 # REPORTING_GEOMETRY_FILE = 'data/grid.shp'
 # OUTPUT_FILE = 'data/grid_with_pop.shp'
-
 INITIAL_REPORTING_VALUE = 0
 INPUT_ATTRIBUTE_TO_REPORT = 'pop'
 INPUT_ATTRIBUTE_TO_REPORT_TYPE = 'float'
 
 input_features = []
+idx = index.Index()
 
 def proportion_of_a_intersecting_b(a, b):
     intersection = a.intersection(b)
     return intersection.area / a.area
 
-def gen(collection):
-    for i, feature in enumerate(collection):
-        s = ShapeWithValue(
-            shape=shape(feature['geometry']),
-            value=feature['properties'][INPUT_ATTRIBUTE_TO_REPORT]
-        )
-        yield((i, s.shape.bounds, s))
-
-
 ShapeWithValue = namedtuple('ShapeWithValue', ['shape', 'value'])
 
 with fiona.drivers():
-    with fiona.open(INPUT_GEOMETRY_FILE, 'r') as input_src:
-        idx = index.Index(gen(input_src))
+    with fiona.open(INPUT_GEOMETRY_FILE) as input_src:
+        for feature in input_src:
+            s = ShapeWithValue(
+                shape=shape(feature['geometry']),
+                value=feature['properties'][INPUT_ATTRIBUTE_TO_REPORT]
+            )
+            input_features.append(s)
+
+    # Populate R-tree index with bounds of input features
+    for pos, feature in enumerate(input_features):
+        idx.insert(pos, feature.shape.bounds)
 
     with fiona.open(REPORTING_GEOMETRY_FILE) as reporting_src:
         sink_schema = reporting_src.schema.copy()
@@ -52,7 +50,7 @@ with fiona.drivers():
                 reporting_value = INITIAL_REPORTING_VALUE
 
                 # look up bbox intersecting features in R-tree
-                intersecting_features = idx.intersection(reporting_shape.bounds, objects="raw")
+                intersecting_features = [input_features[pos] for pos in idx.intersection(reporting_shape.bounds)]
 
                 for input_feature in intersecting_features:
                     # find proportion of input feature that intersects
